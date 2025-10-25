53cb860e2ac1e20d19574a37e23446b1ebcd09b1
SEERAINDU <seeraindu16@gmail.com>
2025-10-25 12:08:58 +0530

    Add files via upload

---
+++ src/data/booksData.js
@@ -0,0 +1,26 @@
+const booksData = [
+  {
+    id: 1,
+    title: "Full Stack Web Development",
+    author: "Stepen Roy",
+    description:
+      "Full-stack development covers the complete spectrum of designing, building, and maintaining applications.",
+    rating: 4.8,
+  },
+  {
+    id: 2,
+    title: "Deep Work",
+    author: "Cal Newport",
+    description: "Rules for focused success in a distracted world.",
+    rating: 4.5,
+  },
+  {
+    id: 3,
+    title: "The Alchemist",
+    author: "Paulo Coelho",
+    description: "A story about following your dreams.",
+    rating: 4.7,
+  },
+];
+
+export default booksData;
